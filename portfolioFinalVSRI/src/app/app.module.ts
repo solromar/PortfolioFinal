9759fdeedd9da17a1c0775ea0a882ae1cbfedd89
Soledad <solromar@gmail.com>
2022-10-20 13:57:02 +0200

    estatico a Angular sin JS

--- portfolioFinalVSRI/src/app/app.module.ts
+++ portfolioFinalVSRI/src/app/app.module.ts
@@ -1,16 +1,43 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './componentes/nav/nav.component';
+import { BannerComponent } from './componentes/banner/banner.component';
+import { AboutComponent } from './componentes/about/about.component';
+import { SkillsComponent } from './componentes/skills/skills.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { PortfolioComponent } from './componentes/portfolio/portfolio.component';
+import { SocialComponent } from './componentes/social/social.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { FormComponent } from './componentes/form/form.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { LoaderComponent } from './componentes/loader/loader.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    BannerComponent,
+    AboutComponent,
+    SkillsComponent,
+    ExperienciaComponent,
+    PortfolioComponent,
+    SocialComponent,
+    EducacionComponent,
+    FormComponent,
+    FooterComponent,
+    LoaderComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
