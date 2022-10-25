cc36bdf84678cfab658fd0a6005d4b4433be2199
Soledad <solromar@gmail.com>
2022-10-25 11:31:59 +0200

    Separacion carpetas
    Separe la carpeta del proyecto en Front y back

---
+++ portfolioFinalVSRI/BackEnd/solromar/src/main/java/com/portfolio/solromar/SolromarApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.solromar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolromarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolromarApplication.class, args);
+	}
+
+}
